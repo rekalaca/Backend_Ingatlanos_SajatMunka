409d6d53a057c4473238b7307ab85a9c26aa5a21
rekalaca <rekalaca@gmail.com>
2022-05-13 15:23:16 +0200

    delete javítva, default date még nincs

--- middlewares/queries.js
+++ middlewares/queries.js
@@ -2,7 +2,7 @@ const connection = require("../services/connection");
 
 
 module.exports.ingatlanList = function (callback) {
-    myQuery = `SELECT ingatlanok.id, kategoriak.nev AS "kategoria", ingatlanok.leiras, DATE_FORMAT(ingatlanok.hirdetesDatuma, "%Y-%m-%d") AS hirdetesDatuma, IF(tehermentes, 'true', 'false') AS "tehermentes", ingatlanok.ar, ingatlanok.kepUrl FROM ingatlanok LEFT JOIN kategoriak ON ingatlanok.id = kategoriak.id`;
+    myQuery = `SELECT ingatlanok._id, kategoriak.nev AS "kategoria", ingatlanok.leiras, DATE_FORMAT(ingatlanok.hirdetesDatuma, "%Y-%m-%d") AS hirdetesDatuma, IF(tehermentes, 'true', 'false') AS "tehermentes", ingatlanok.ar, ingatlanok.kepUrl FROM ingatlanok LEFT JOIN kategoriak ON ingatlanok._id = kategoriak.id`;
     connection.query(myQuery, (err, result, fields) => {
       if (err) callback(err, null);
       else {
@@ -12,7 +12,7 @@ module.exports.ingatlanList = function (callback) {
   };
 
   module.exports.addIngatlan = function (data, callback) {
-    myQuery = `INSERT INTO ingatlanok (id, kategoria, leiras, hirdetesDatuma, tehermentes, ar, kepUrl) VALUES ('${data._id}','${data.kategoria}','${data.leiras}','${data.hirdetesDatuma}','${data.tehermentes}','${data.ar}','${data.kepUrl}')`;
+    myQuery = `INSERT INTO ingatlanok (_id, kategoria, leiras, hirdetesDatuma, tehermentes, ar, kepUrl) VALUES ('${data._id}','${data.kategoria}','${data.leiras}','${data.hirdetesDatuma}','${data.tehermentes}','${data.ar}','${data.kepUrl}')`;
     connection.query(myQuery, (err, result, fields) => {
       if (err) callback(err.sqlMessage, null);
       else {
@@ -21,8 +21,8 @@ module.exports.ingatlanList = function (callback) {
     });
   };
 
-  module.exports.delIngatlan = function (data, callback) {
-    myQuery = `DELETE FROM ingatlanok WHERE id = '${data.id}'`;
+  module.exports.delIngatlan = function (_id, callback) {
+    myQuery = `DELETE FROM ingatlanok WHERE _id = '${_id}'`;
     connection.query(myQuery, (err, result, fields) => {
       if (err) callback(err, null);
       else {
